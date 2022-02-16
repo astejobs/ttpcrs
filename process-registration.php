7aaaaa9800b5a92aca8186947fcd9f9c0fc2af80
shah-Shahid <shahid.sheikhpora@gmail.com>
2022-02-16 16:04:53 +0530

    errer Reporting Added

--- process-registration.php
+++ process-registration.php
@@ -1,4 +1,11 @@
 <?php 
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+/*** THIS! ***/
+mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT);
+/*** ^^^^^ ***/
+
 require_once('connection.php');
 //Update Existing News Blog 
 if(isset($_GET['edit'])){
