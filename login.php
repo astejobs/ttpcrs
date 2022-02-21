a008acf4a7a2218276592a8769ae90e086dfe5a8
shah-Shahid <shahid.sheikhpora@gmail.com>
2022-02-21 10:19:39 +0530

    login

--- login.php
+++ login.php
@@ -88,7 +88,7 @@ if(isset($_POST['login'])) {
 					<div class="mb-3">
 						<label for="exampleInputEmail1" class="form-label">Username</label>
 						<input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="username">
-						<div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+						<!-- <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div> -->
 					</div>
 					<div class="mb-3">
 						<label for="exampleInputPassword1" class="form-label">Password</label>
